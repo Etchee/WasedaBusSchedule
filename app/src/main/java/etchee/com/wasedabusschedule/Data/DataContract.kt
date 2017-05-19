6544a24b116e9be73b6450c87913c4fc9ab67363
rikutoechigoya <rikutoechigoya@fuji.waseda.jp>
2017-05-19 15:16:22 +0900

    Playing with Kotlin

---
+++ app/src/main/java/etchee/com/wasedabusschedule/Data/DataContract.kt
@@ -0,0 +1,12 @@
+package etchee.com.wasedabusschedule.Data
+
+/**
+ * Created by rikutoechigoya on 2017/05/19.
+ */
+
+class DataContract {
+
+    internal inner class GlobalConstants {
+        val PASS_STRING = "KEY_STRING"
+    }
+}
--- app/src/main/java/etchee/com/wasedabusschedule/MainActivity.kt
+++ app/src/main/java/etchee/com/wasedabusschedule/MainActivity.kt
@@ -1,14 +1,26 @@
 package etchee.com.wasedabusschedule
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import etchee.com.wasedabusschedule.Data.DataContract
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button = findViewById(R.id.button) as Button
+        button.setOnClickListener { _ ->
+            val intent = Intent(this, Main2Activity::class.java)
+            val bundle = Bundle()
+            bundle.putString(DataContract.GlobalConstants:: PASS_STRING.toString(), "Hello")
+            startActivity(intent)
+        }
     }
 
-
 }
+
+

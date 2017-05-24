ca3fbaa0d7d81c93f954ecb5897ec5a7a5d8b2fa
rikutoechigoya <rikutoechigoya@fuji.waseda.jp>
2017-05-24 10:59:50 +0900

    ViewPager Adapter

--- app/src/main/java/etchee/com/wasedabusschedule/ListViewPagerAdapter.kt
+++ app/src/main/java/etchee/com/wasedabusschedule/ListViewPagerAdapter.kt
@@ -4,6 +4,9 @@ import android.content.Context
 import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentManager
 import android.support.v4.app.FragmentStatePagerAdapter
+import android.util.Log
+import etchee.com.wasedabusschedule.Fragments.ToNishiFragment
+import etchee.com.wasedabusschedule.Fragments.ToWasedaFragment
 
 /**
  * Adapter for viewPager
@@ -14,16 +17,52 @@ class ListViewPagerAdapter(fm: FragmentManager, private val context: Context) : 
 
     private val TAG = javaClass.simpleName
 
+    //return appropriate fragment
     override fun getItem(position: Int): Fragment? {
-        return null
+
+        when(position){
+
+            //toWasedaFragment
+            0 -> {
+                return ToWasedaFragment()
+            }
+
+            //toNishiFragment
+            1 -> {
+                return ToNishiFragment()
+            }
+
+            //impossible viewPager index
+            else -> {
+                Log.e(TAG, "ViewPagerAdapter did not recognize the item number")
+                return null
+            }
+        }
     }
 
     //return the current number of item
     override fun getCount(): Int {
-        return 0
+        return 2
     }
 
     override fun getPageTitle(position: Int): CharSequence {
-        return ""
+        when(position){
+
+        //toWasedaFragment
+            0 -> {
+                return "早稲田行き"
+            }
+
+        //toNishiFragment
+            1 -> {
+                return "西早稲田行き"
+            }
+
+        //impossible viewPager index
+            else -> {
+                Log.e(TAG, "ViewPagerAdapter did not recognize the item number")
+                return ""
+            }
+        }
     }
 }

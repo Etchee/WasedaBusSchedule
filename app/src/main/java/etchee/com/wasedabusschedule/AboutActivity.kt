640d9e5ced518590d889ffa6ed01f42e91bb4a25
rikuto_bigarms <rikutoechigoya@fuji.waseda.jp>
2017-06-20 17:16:55 +0900

    About display completed

--- app/src/main/java/etchee/com/wasedabusschedule/AboutActivity.kt
+++ app/src/main/java/etchee/com/wasedabusschedule/AboutActivity.kt
@@ -3,6 +3,7 @@ package etchee.com.wasedabusschedule
 import android.os.Bundle
 import android.os.PersistableBundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
 import android.widget.ImageView
 import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.layout_about.*
@@ -16,5 +17,9 @@ class AboutActivity: AppCompatActivity() {
         setContentView(R.layout.layout_about)
 
         setSupportActionBar(about_toolBar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
+        about_toolBar.setNavigationOnClickListener({super.onBackPressed()})
+
     }
 }
